8fe09a2c71660e42fa9a08fb252356b2ae3b2704
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2023-11-06 22:39:09 +0600

    middleware and utils

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
-import connectDB from './config/database.js'
 import express from 'express';
+import connectDB from './config/database.js';
 const app = express();
 
 dotenv.config({
@@ -8,3 +8,11 @@ dotenv.config({
 });
 
 connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server in running at port:${process.env.PORT}`)
+        });
+    })
+    .catch((error) => {
+        console.log(`MONGODB connection failed !!`, error);
+    });
